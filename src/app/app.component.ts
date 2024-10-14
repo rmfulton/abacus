b0cbb9465edec185fbce2fd9ada851cac364f4b8
rmfulton <rmfulton@gmail.com>
2024-10-13 22:48:04 -0700

    started creating the app!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import {Component} from '@angular/core';
-
+import {BoardComponent } from './board/board.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `
-    <h1>Default</h1>
-  `,
+  imports: [BoardComponent],
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'default';
+  title = 'homes';
 }
---
+++ src/app/board/board.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent {
+
+}
